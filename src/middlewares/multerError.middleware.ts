90035a44454706f4879c2d3811666b7a3c56b2b7
Novpa <aa.novparadiputra@binus.ac.id>
2026-05-17 14:21:34 +0800

    feat: add configuration files and middleware for cloudinary, cors, prisma error handling, global error handling. & defining folder structure based on best practices

---
+++ src/middlewares/multerError.middleware.ts
@@ -0,0 +1,25 @@
+// MULTER MIDDLEWARE FOR UPLOAD MANY
+
+import { ErrorRequestHandler } from "express";
+import { MulterError } from "multer";
+import { AppError } from "../utils/appErrror.util";
+import { MAX_PRODUCT_UPLOAD_IMG } from "../statics/multer.statics";
+
+export const multerErrorMiddleware: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
+) => {
+  console.log("ERR", err);
+  if (err instanceof MulterError && err.code === "LIMIT_UNEXPECTED_FILE") {
+    throw new AppError(
+      400,
+      `Maximum upload is ${MAX_PRODUCT_UPLOAD_IMG} file only`,
+    );
+  } else if (err) {
+    throw new AppError(400, `There is an issue during uploading`);
+  } else {
+    next();
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,44 @@
-import express, { Express } from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+import { CORS_CONFIG } from "./configs/cors.config";
+import { DATABASE_CREDENTIALS } from "./configs/dotenv.config";
+import { globalErrorHandler } from "./middlewares/error.middleware";
+import { upload } from "./configs/multer.config";
+import { uploadMany, uploadSingle } from "./utils/cloudinaryUploader.util";
+import { multerErrorMiddleware } from "./middlewares/multerError.middleware";
 
+const port = DATABASE_CREDENTIALS.PORT;
 const app: Express = express();
 app.use(express.json());
-app.use(cors());
 
-const port = process.env.PORT;
+// cookie-parser middleware
+app.use(cookieParser());
+
+// cors
+app.use(cors(CORS_CONFIG));
+
+// END-POINTS
+app.post(
+  "/api/upload-testing",
+  upload.array("upload-multiple-testing", 2),
+  multerErrorMiddleware,
+  async (req: Request, res: Response) => {
+    const uploadedFiles = req.files as Express.Multer.File[];
+
+    const urls = await uploadMany(uploadedFiles, "upload-multiple-testing");
+    res.status(200).json({
+      status: "success",
+      urls,
+      message: "testing successfull",
+    });
+  },
+);
+
+// globar error middleware
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`🦄 🌱 [server]: Server is running at http://localhost:${port}`);
 });
