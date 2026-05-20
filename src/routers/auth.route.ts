9225cd83e4d31f3915cc6ea8729906633eb10b28
Novpa <aa.novparadiputra@binus.ac.id>
2026-05-20 21:11:15 +0800

    feat: implement signup validation and error handling

--- src/routers/auth.route.ts
+++ src/routers/auth.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authController } from "../controllers/auth.controller";
+import { validate } from "../controllers/validation.middleware";
+import { signupSchema } from "../schemas/signup.schema";
 
 const route = Router();
 
-route.post("/signup", authController.signup);
+route.post("/signup", validate(signupSchema), authController.signup);
 
 export default route;
