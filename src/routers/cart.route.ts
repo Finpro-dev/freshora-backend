869891b1af65af0fdd881dae1db4185c98e4d860
Novpa <aa.novparadiputra@binus.ac.id>
2026-05-23 19:57:37 +0800

    ref: streamline route definitions in cart.route.ts

--- src/routers/cart.route.ts
+++ src/routers/cart.route.ts
@@ -1,23 +1,17 @@
 import { Router } from "express";
 import { cartController } from "../controllers/cart.controller";
-import { validate } from "../controllers/validation.middleware";
+
 import { addToCartSchema, updateCartSchema } from "../schemas/cart.schema";
 import { authentication, authorization } from "../middlewares/auth.middleware";
+import { validate } from "../middlewares/validation.middleware";
 
 const route = Router();
 
 route.use(authentication, authorization("CUSTOMER"));
 
-route.get(
-  "/",
-  cartController.getAllCart,
-);
+route.get("/", cartController.getAllCart);
 
-route.post(
-  "/items",
-  validate(addToCartSchema),
-  cartController.addToCart,
-);
+route.post("/items", validate(addToCartSchema), cartController.addToCart);
 
 route.put(
   "/items/:cartItemId",
@@ -25,14 +19,8 @@ route.put(
   cartController.updateCartItem,
 );
 
-route.delete(
-  "/items/:cartItemId",
-  cartController.removeCartItem,
-);
+route.delete("/items/:cartItemId", cartController.removeCartItem);
 
-route.get(
-  "/count",
-  cartController.getCartCount,
-);
+route.get("/count", cartController.getCartCount);
 
 export default route;
