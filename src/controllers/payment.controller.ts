434e153d1323bc7f6560d6e84493dd37308e5821
Novpa <aa.novparadiputra@binus.ac.id>
2026-05-25 22:39:13 +0800

    feat: update payment model and implement payment status update logic

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -2,6 +2,8 @@ import { Response } from "express";
 import { AuthenticatedRequest } from "../types/appRequest.type";
 import { paymentService } from "../services/payment.service";
 import { catchAsync } from "../utils/catchAsync.util";
+import { prisma } from "../configs/prisma.config";
+import { mapMidtransToPaymentStatus } from "../utils/midtransPaymentMapper";
 
 export const paymentController = {
   createPayment: async (req: AuthenticatedRequest, res: Response) => {
@@ -18,6 +20,32 @@ export const paymentController = {
     async (req: AuthenticatedRequest, res: Response) => {
       console.log("MIDTRANS REQ.BODY ==>", req.body);
 
+      const paymentStatus = req.body.transaction_status;
+      const transactionId = req.body.order_id;
+
+      const status = mapMidtransToPaymentStatus(paymentStatus);
+
+      await prisma.payment.update({
+        where: {
+          transactionId,
+        },
+
+        data: {
+          paymentStatus: status,
+        },
+      });
+
+      if (status === "SETTLEMENT") {
+        await prisma.storeOrder.updateMany({
+          where: {
+            transactionId,
+          },
+
+          data: {
+            transactionStatus: "PROCESSING",
+          },
+        });
+      }
       res.status(200).json({
         status: "success",
         message: "Payment updated successfully",
