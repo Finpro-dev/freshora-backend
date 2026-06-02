62cd12b49f26c746150b9761b4d2718819dc82f6
RizanZun <fakhrizaanalhakim.ia@gmail.com>
2026-06-02 10:14:20 +0700

    feat: update getAllUsers service to support pagination and role filtering

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -1,31 +1,57 @@
 import { prisma } from "../configs/prisma.config";
 
 export const adminServices = {
-  getAllUsers: async () => {
-    const users = await prisma.user.findMany({
-      where: {
-        deletedAt: null,
-      },
-      select: {
-        userId: true,
-        firstName: true,
-        lastName: true,
-        email: true,
-        phone: true,
-        gender: true,
-        role: true,
-        isVerified: true,
-        avatar: true,
-        myReferralCode: true,
-        usedReferralCode: true,
-        createdAt: true,
-        updatedAt: true,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+  getAllUsers: async (query: any) => {
+    const page = Number(query.page) || 1;
+    const limit = Number(query.limit) || 10;
+    const role = query.role;
+    const skip = (page - 1) * limit;
+    const allowedRoles = ["CUSTOMER", "STORE_ADMIN"]; // filter role yang diperbolehkan
+    const where: any = {
+      deletedAt: null,
+    };
+    if (role && allowedRoles.includes(role)) {
+      where.role = role;
+    }
 
-    return users;
+    const [users, total] = await Promise.all([
+      prisma.user.findMany({
+        where,
+        skip,
+        take: limit,
+        select: {
+          userId: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          phone: true,
+          gender: true,
+          role: true,
+          isVerified: true,
+          avatar: true,
+          myReferralCode: true,
+          usedReferralCode: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      }),
+
+      prisma.user.count({
+        where,
+      }),
+    ]);
+
+    return {
+      users,
+      meta: {
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   },
 };
